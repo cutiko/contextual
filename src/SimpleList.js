58dcaf50a726f6749145909307c30649bd9f0718
cutiko <cutiko@gmail.com>
2018-06-01 12:35:28 -0400

    ADD: basic unmounter

--- src/SimpleList.js
+++ src/SimpleList.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Updater from "./Updater";
 import Visualizer from "./Visualizer";
 
-function SimpleList(props) {
-    return (
-        <ul>
-            <Updater/>
-            <Visualizer/>
-        </ul>
-    );
+class SimpleList extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            listening: true
+        };
+    }
+
+    isVisualizer() {
+        if (this.state.listening) {
+            return <Visualizer/>
+        }
+        return <li>Listening has stop</li>
+    }
+
+    render() {
+        return (
+            <ul>
+                <Updater/>
+                {this.isVisualizer()}
+                <li><button onClick={()=>this.setState({listening:false})}>Unmount</button></li>
+            </ul>
+        );
+    }
+
 }
 
 export default SimpleList;
