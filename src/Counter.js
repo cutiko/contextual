6c9e0a06e19d09182e3fe37dc42abd776116b7df
cutiko <cutiko@gmail.com>
2018-06-01 11:03:30 -0400

    ADD: React component is more suitable

--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 import {ThemeContext} from "./theme-context";
 
-function Counter(props) {
-    return (
-        <ThemeContext.Consumer>
-            {rtd => (
-                <li onClick={()=>rtd.ref('consumer').set('is working')}>CLICK
-                </li>
-            )}
-        </ThemeContext.Consumer>
-    );
-}
+export default class Counter extends Component {
 
-export default Counter;
+    defineRender(rtd) {
+        if (!rtd) {
+            return (<li>Wait Please</li>);
+        }
+        return (
+            <li onClick={()=>rtd.ref('consumer').set(Date.now())}><button>Click to update</button></li>
+        );
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <ThemeContext.Consumer>
+                    {rtd => (
+                        this.defineRender(rtd)
+                    )}
+                </ThemeContext.Consumer>
+            </Fragment>
+        );
+    }
+
+}
