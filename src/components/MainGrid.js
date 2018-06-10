3ed3a2110268d97409d3acc12f0a42955499c1c5
cutiko <cutiko@gmail.com>
2018-06-10 18:43:22 -0400

    ADD: sign up as a subscriber

--- src/components/MainGrid.js
+++ src/components/MainGrid.js
@@ -1,43 +1,18 @@
-import React, {Component, Fragment} from 'react';
-import LeftColumn from './columns/LeftColumn';
-import RightColumn from './columns/RightColumn';
-import Login from '../subscribers/Login'
-import user from "../consumers/user";
+import React, {Component} from 'react';
+import SignUp from '../subscribers/SignUp'
 
-class MainGrid extends Component {
+export default class MainGrid extends Component {
 
-    isAuth() {
-        const WrappedAuth = user(authComponent);
-        return <WrappedAuth/>;
-    }
 
     render() {
         return (
             <div className="flex flex-wrap p-3">
-                {this.isAuth()}
+                <SignUp/>
+                <div className="p-4 m-4 text-center bg-teal-lightest w-full">You can open the console to see more
+                    details
+                </div>
             </div>
         );
     }
 
 }
-
-function authComponent(props) {
-    if (props.currentUser) {
-        return (
-            <Fragment>
-                <LeftColumn
-                    title={"Real Time Example"}
-                    content={"The component can send the current time to the Firebase RTD and stay listening to it. When is unmount the listener will be shut down and turn on again when is mounted."}/>
-                <RightColumn
-                    title={"One Time Example"}
-                    content={"The component can send the current time to the Firebase RTD but only listen once. You can send the value again, too see it: unmoun and re-attach"}/>
-                <div className="p-4 m-4 text-center bg-teal-lightest w-full">You can open the console to see more
-                    details
-                </div>
-            </Fragment>
-        );
-    }
-    return <Login/>;
-}
-
-export default MainGrid;
---
+++ src/subscribers/SignUp.js
@@ -0,0 +1,32 @@
+import React, {Component, Fragment} from 'react';
+import user from "../consumers/user";
+import LeftColumn from '../components/columns/LeftColumn'
+import RightColumn from '../components/columns/RightColumn'
+import Login from '../subscribers/Login'
+
+class SignUp extends Component {
+
+    render() {
+        const currentUser = this.props.currentUser;
+        if (currentUser) {
+            return (
+                <Fragment>
+                    <LeftColumn
+                        title={"Real Time Example"}
+                        content={"The component can send the current time to the Firebase RTD and stay listening to it. When is unmount the listener will be shut down and turn on again when is mounted."}/>
+                    <RightColumn
+                        title={"One Time Example"}
+                        content={"The component can send the current time to the Firebase RTD but only listen once. You can send the value again, too see it: unmoun and re-attach"}/>
+                </Fragment>
+            );
+        } else {
+            return(
+                <Login/>
+            );
+        }
+
+    }
+
+}
+
+export default user(SignUp);
