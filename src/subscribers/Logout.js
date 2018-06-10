c2ce71e9c6109505a4b90176365170eb1ff65e76
cutiko <cutiko@gmail.com>
2018-06-10 18:52:39 -0400

    ADD: logout functionality

--- src/subscribers/Logout.js
+++ src/subscribers/Logout.js
@@ -3,9 +3,16 @@ import auth from "../consumers/auth";
 
 class Logout extends Component{
 
+    logout() {
+        const auth = this.props.auth;
+        if (auth) {
+            auth.signOut();
+        }
+    }
+
     render() {
         return (
-            <div className="p-4 m-4 text-center bg-grey-light w-full underline hover:text-blue cursor-pointer">
+            <div onClick={()=>this.logout()} className="p-4 m-4 text-center bg-grey-light w-full underline hover:text-blue cursor-pointer">
                 Logout
             </div>
         );
