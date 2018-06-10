3b39efb1e20767e8d8e3265f0116e175c63f8176
cutiko <cutiko@gmail.com>
2018-06-10 15:40:30 -0400

    ADD: basic login

--- src/subscribers/Login.js
+++ src/subscribers/Login.js
@@ -3,9 +3,18 @@ import auth from "../consumers/auth";
 
 class Login extends Component{
 
+    createUser() {
+        this.props.auth.createUserWithEmailAndPassword("test@test.com", "12345678").catch(function(error) {
+            console.log("LOGIN ERROR:", error);
+        });
+    }
+
     render() {
         return (
-            <p>No user</p>
+            <button
+                onClick={()=> this.createUser()}
+                className="bg-green hover:bg-green-dark text-white font-bold py-2 px-4 rounded"
+            >CREATE TEST USER</button>
         );
     }
 
