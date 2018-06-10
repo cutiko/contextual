21354954f64ee960d542013f3ef7c9dca5659b8f
cutiko <cutiko@gmail.com>
2018-06-09 22:12:38 -0400

    ADD: handle date

--- src/components/timeItem.js
+++ src/components/timeItem.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default function (props) {
     return (
-        <h4 className="text-center m-8">Last saved time: {props.text}</h4>
+        <h4 className="text-center m-8">Last saved time: {convertDate(props.text)}</h4>
     );
+}
+
+function convertDate(time) {
+    console.log(typeof time);
+    if (typeof time === "string") {
+        return time;
+    }
+    return new Date(time).toString()
 }
