8a9dc810bc45f28698d867e6487ca53143db1d90
cutiko <cutiko@gmail.com>
2018-06-10 19:10:24 -0400

    ADD: navbar

--- src/components/MainGrid.js
+++ src/components/MainGrid.js
@@ -1,17 +1,21 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import SignUp from '../subscribers/SignUp'
+import NavBar from './navBar';
 
 export default class MainGrid extends Component {
 
 
     render() {
         return (
-            <div className="flex flex-wrap p-3">
-                <SignUp/>
-                <div className="p-4 m-4 text-center bg-teal-lightest w-full">You can open the console to see more
-                    details
+            <Fragment>
+                <NavBar/>
+                <div className="flex flex-wrap p-3">
+                    <SignUp/>
+                    <div className="p-4 m-4 text-center bg-teal-lightest w-full">You can open the console to see more
+                        details
+                    </div>
                 </div>
-            </div>
+            </Fragment>
         );
     }
 
---
+++ src/components/navBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function (props) {
+
+    return(
+        <div className="bg-blue flex flex-wrap p-4 text-white font-bold">
+            <div className="w-1/2 sm:w-1/2 lg:w-3/4">
+                Welcome to Contextual
+            </div>
+
+        </div>
+    );
+}
